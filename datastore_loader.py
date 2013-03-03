1b20423dc1964af2d7a7f4f22398838d44b402bb
Joshua Tauberer <jt@occams.info>
2013-03-03 16:35:06 -0500

    more work

--- datastore_loader.py
+++ datastore_loader.py
@@ -1,7 +1,10 @@
 # Loads CKAN resources into the CKAN Datastore
 # to make an API out of static files.
 
-import argparse, urllib2, json, os.path, re, unicodedata, logging
+import argparse, logging
+import os.path, re, unicodedata
+import urllib2, json, cStringIO, hashlib
+from datetime import datetime
 
 # Command-line arguments
 
@@ -9,6 +12,8 @@ parser = argparse.ArgumentParser(description='Load a CKAN resource into the Data
 parser.add_argument('base_url', type=str, help='The CKAN base URL, e.g. http://www.example.org')
 parser.add_argument('api_key', type=str, help='A CKAN API key that can edit the resource.')
 parser.add_argument('resource_id', type=str, nargs="?", help='The resource GUID, or omit to load all resources in the CKAN catalog.')
+parser.add_argument('--cache', action="store_true", help='Cache the resource data file locally to make testing faster.')
+parser.add_argument('--ifchanged', action="store_true", help='Only load resources into the datastore if the content has changed.')
 args = parser.parse_args()
 
 # Configure Logging
@@ -88,15 +93,32 @@ def ckan(action, params, error_handler=None):
 # Main routines.
 
 def upload_resource_to_datastore(resource):
-	# Given a resource (passed as a dict that has at least
-	# id and url), downloads the resource's raw content,
-	# parses it into a table, and uploads the table to
-	# the CKAN Datastore.
+	# This is the main function of this module. It takes a
+	# resource (a dict as returned from resource_show), downloads
+	# the content, parses the content as a table, and uploads
+	# it into the CKAN datastore.
 
-	log.info("Processing %s (%s)." % (resource["url"], resource["id"]))
+	# Download the resource.
+	resource_file, mime_type, fileext = load_resource_content(resource)
+	
+	# Get the SHA1 hash of the resource content.
+	sha = hashlib.sha1()
+	sha.update(resource_file)
+	resource_hash = sha.hexdigest()
+	
+	# If --ifchanged is used, only upload to the datastore if we
+	# have a new file.
+	if args.ifchanged:
+		if resource.get("datastore_content_hash", None) == resource_hash:
+			log.info("Skipping resource --- not changed.")
+			return
 
-	# Load the resource so we know the column headers, datatypes, etc.
-	schema, recorditer = parse_resource(resource)
+	# From here on, resource_file is expected to be a file-like obj.
+	resource_file = cStringIO.StringIO(resource_file)
+	
+	# Parse the resource to get the file format, column headers, datatypes, etc.
+	# recorditer is a sequence of rows in the table, as returned from messytables.
+	schema, recorditer = parse_resource(resource_file, mime_type, fileext)
 	
 	# Upload the resource to the Datastore.
 	try:
@@ -107,22 +129,78 @@ def upload_resource_to_datastore(resource):
 		# to pass the inferred schema back to the caller so that
 		# the user can edit the schema to try to avoid the error.
 		raise
+		
+	# After a successful load, write the file hash into the resource.
+	resource["datastore_content_hash"] = resource_hash
+	resource["datastore_last_updated"] = datetime.utcnow().isoformat() # CKAN assumes strings that look like dates are in UTC
+	resource["datastore_schema"] = json.dumps(schema, sort_keys=True, indent=4)
+	ckan("resource_update", resource)
 	
-def parse_resource(resource):
-	# Given a resource (passed as a dict that has at least
-	# id and url), returns a tuple of
-	#  * the schema used to load the file
-	#  * the resource as a table, as given by messytables (an iterator over rows) 
+def load_resource_content(resource):
+	# Downloads the content of the resource file (resource["url"]).
+	#
+	# While it might be nice to support streaming the resource
+	# throughout, there are too many cases when we really need
+	# the whole thing in memory. So we return it as a str.
 	
+	# If --cache is used, see if we already have this resource in the cache.
+	# If so, return it from the cache.
+	if args.cache:
+		import sqlite3, base64
+		conn = sqlite3.connect('cache.db')
+		c = conn.cursor()
+		try:
+			c.execute("CREATE TABLE cache (url TEXT UNIQUE, content BLOB, mimetype TEXT, fileext TEXT)")
+		except sqlite3.OperationalError as e:
+			if str(e) != "table cache already exists":
+				raise
+		c.execute("SELECT content, mimetype, fileext FROM cache WHERE url=?", (resource["url"],))
+		cache_hit = c.fetchone()
+		if cache_hit:
+			content, mime_type, fileext = cache_hit
+			return base64.b64decode(content), mime_type, fileext
+		
 	# Start a download of the resource. The actual download
 	# will probably commence inside AnyTableSet.from_fileobj
 	# when .read() is called.
-	payload = urllib2.urlopen(resource["url"])
+	log.info("Downloading %s..." % resource["url"])
+	try:
+		resource_file = urllib2.urlopen(resource["url"])
+	except urllib2.HTTPError as e:
+		raise UserError("Error downloading resource: " + str(e))
+
+	# Get the MIME type and the file extension, in case we
+	# are auto-detecting the file format.
+	mime_type = resource_file.info()["Content-Type"]
+	filename, fileext = os.path.splitext(resource["url"])
+	if fileext.strip() in ("", "."):
+		fileext = None
+	else:
+		fileext = fileext[1:] # strip off '.'
+		
+	resource_file = resource_file.read()
+
+	if args.cache:
+		# Store the value in the cache. We'll need to slurp the
+		# resource into memory, then wrap it back in a file-like
+		# object before we return it.
+		c = conn.cursor()
+		c.execute("DELETE FROM cache WHERE url=?", (resource["url"],))
+		c.execute("INSERT INTO cache VALUES(?,?,?,?)", (resource["url"], base64.b64encode(resource_file), mime_type, fileext))
+		conn.commit()
+		
+	return resource_file, mime_type, fileext
+
+def parse_resource(resource_file, mime_type, fileext):
+	# Given resource data, returns a tuple of
+	#  * the schema used to load the file
+	#  * the resource as a table, as given by messytables (an iterator over rows) 
 	
 	# Schema defaults. We'll build up the schema with defaults
 	# from the actual resource so that it is easy for the data
 	# owner to customize the schema later.
 	schema = {
+		"_format": 1,
 	}
 	
 	# Utility function that's like dict.get() but works on nested
@@ -154,7 +232,7 @@ def parse_resource(resource):
 			container = container.setdefault(p, {})
 		container[field] = value
 	
-	# Parse the payload.
+	# Parse the resource_file.
 	
 	# Get the table data format.
 	
@@ -191,7 +269,7 @@ def parse_resource(resource):
 		raise UserError("Invalid format name in schema. Allowed values are: csv, tsv.")
 		
 	# If the user specifies a ZIP container, then parse the
-	# payload as a ZIP file and pass the format parameters
+	# resource_file as a ZIP file and pass the format parameters
 	# into ZIPTableSet so it knows how to parse the inner files.
 	
 	if schema_get("container", "name") == "zip":
@@ -200,7 +278,7 @@ def parse_resource(resource):
 		# }
 		
 		from messytables import ZIPTableSet
-		table_set = ZIPTableSet.from_fileobj(payload,
+		table_set = ZIPTableSet.from_fileobj(resource_file,
 			inner_data_format=data_format,
 			inner_parser_args=data_format_args)
 
@@ -211,23 +289,15 @@ def parse_resource(resource):
 	# Just parse according to the specified format.
 
 	elif schema_get('format', 'name') != None:
-		table_set = data_format.from_fileobj(payload, **data_format_args)
+		table_set = data_format.from_fileobj(resource_file, **data_format_args)
 		
 	# If no container and no format was specified, auto-detect everything.
 	
 	else:
-		# Get the MIME type and the file extension.
-		mime_type = payload.info()["Content-Type"]
-		filename, fileext = os.path.splitext(resource["url"])
-		if fileext.strip() in ("", "."):
-			fileext = None
-		else:
-			fileext = fileext[1:] # strip off '.'
-		
 		# Use the AnyTableSet to guess all parsing parameters.
 		from messytables import AnyTableSet
 		try:
-			table_set = AnyTableSet.from_fileobj(payload, mimetype=mime_type, extension=fileext)
+			table_set = AnyTableSet.from_fileobj(resource_file, mimetype=mime_type, extension=fileext)
 		except Exception as e:
 			raise UserError("The file format could not be recognized: %s" % str(e))
 		
@@ -259,9 +329,9 @@ def parse_resource(resource):
 	schema_set("header", "offset", offset)
 	
 	# Override the header names with what is specified in the schema.
-	for cidx in schema_get("columns", default={}):
+	for cidx, col in enumerate(schema_get("columns", default=[])):
 		try:
-			headers[cidx] = schema_get("columns", cidx, "name", default=headers[cidx])
+			headers[cidx] = col.get("name", headers[cidx])
 		except IndexError:
 			pass # ignore schema problems?
 	
@@ -270,8 +340,8 @@ def parse_resource(resource):
 	# we're at it, also trim spaces.
 	headers = [h.lower().strip() for h in headers]
 	
-	# Ensure the headers are valid SQL-ish column names:
-	#  1st character: letter or underscore
+	# Ensure the headers are valid SQL-ish & datastore column names:
+	#  1st character: letter
 	#  subsequent characters: letter, number, or underscore
 	for i, header in enumerate(headers):
 		# To play nice with international characters, convert to ASCII
@@ -280,13 +350,13 @@ def parse_resource(resource):
 		header = u"".join(c for c in unicodedata.normalize('NFKD', header)
 			if not unicodedata.combining(c))
 		
-		# If there is an invalid 1st character, prepend an underscore.
-		if not re.match("^[a-z_]", header):
-			header = "_" + header
-			
 		# Replace any invalid characters with "".
 		header = re.sub("[^a-z0-9_]", "", header)
 		
+		# If there is an invalid 1st character, prepend a valid start.
+		if not re.match("^[a-z]", header):
+			header = "field_" + header
+			
 		# And force to an ASCII byte string, which should be possible by now.
 		headers[i] = str(header)
 
@@ -298,10 +368,6 @@ def parse_resource(resource):
 	from messytables import headers_make_unique
 	headers = headers_make_unique(headers, max_length=24)
 	
-	# Provide the header names to the user in the schema.
-	for i in xrange(len(headers)):
-		schema_set("columns", i, "name", headers[i])
-
 	# Skip the header row.
 	# (Add one to begin with content, not the header.)
 	from messytables import offset_processor
@@ -332,26 +398,25 @@ def parse_resource(resource):
 	datatypes = [messytable_datastore_type_mapping[type(t)] for t in datatypes] # convert objects to strings
 	
 	# Override the datatypes from the schema.
-	for cidx in schema_get("columns", default={}):
+	for cidx, col in enumerate(schema_get("columns", default=[])):
 		try:
-			datatypes[cidx] = schema_get("columns", cidx, "type", default=datatypes[cidx])
+			datatypes[cidx] = col.get("type", datatypes[cidx])
 		except IndexError:
 			pass # ignore schema problems?
 	
-	# Provide the column data type names to the user in the schema.
+	# Provide the header names and types back to the user in the schema.
+	schema["columns"] = []
 	for i in xrange(len(headers)):
-		schema_set("columns", i, "type", datatypes[i])
+		schema["columns"].append({
+			"name": headers[i],
+			"type": datatypes[i],
+		})
 		
 	# Validate that the datatypes are all legit.
 	for dt in datatypes:
 		if dt not in ("text", "int", "float", "bool", "numeric", "date", "time", "timestamp", "json"):
 			raise UserError("Invalid data type in schema: %s" % dt)
 			
-	# Validate that every column has information in the schema.
-	for i in xrange(max(schema["columns"])+1):
-		if i not in schema["columns"]:
-			raise UserError("The schema is missing information for column %d." % i)
-
 	return schema, table
 
 def upload_resource_records(resource, schema, recorditer):
@@ -372,7 +437,7 @@ def upload_resource_records(resource, schema, recorditer):
 			{
 				"id": col["name"],
 				"type": col["type"],
-			} for cidx, col in sorted(schema["columns"].items()) ]
+			} for cidx, col in enumerate(schema["columns"]) ]
 		})
 		# TODO: also send primary_key, indexes?
 	
@@ -387,18 +452,13 @@ def upload_resource_records(resource, schema, recorditer):
 		if len(chunk) > 0:
 			yield chunk			
 			
-	# Helper function to format the raw string value from the file
-	# in a format appropriate for the JSON call to the API, which
-	# will pass the value off to PostreSQL. Validates the content.
-	def format_value(cellvalue, datatype, rownum, colnum, colname):
+	# Helper function to validate that the raw string value from the file
+	# is in a format appropriate for the JSON call to the API, which
+	# will pass the value off to PostreSQL.
+	def validate_cell(cell, datatype, rownum, colnum, colname):
 		# Return value must be JSON-serializable so we can pass it
 		# through the API. Then datastore had better know how to
 		# convert that into a string for the SQL statement.
-		
-		# The empty string is invalid for columns besides text,
-		# unless we treat it as a database NULL.
-		if cellvalue.strip() == "" and datatype != "text":
-			return None # db NULL
 		
 		# Get the type converter for the column's datatype.
 		import messytables.types
@@ -410,14 +470,32 @@ def upload_resource_records(resource, schema, recorditer):
 			'timestamp': messytables.types.DateType,
 		}
 		typ = datastore_messytable_type_mapping[datatype]
+
+		if cell.type != None:
+			# The XLS parser does type conversion for us. Just check
+			# that the cell datatype matches the column datatype.
+			if type(cell.type) != typ:
+				raise UserError('The %s value %s in row %d column %d (%s) is invalid for a %s column.' % (str(cell.type), repr(cell.value), rownum+1, colnum+1, colname, datatype))
+			
+			# If it matches, return the value unchanged. We're relying
+			# on the assumption that the casted value according to the
+			# messytables datatype will be valid for the column in the
+			# datastore, otherwise we may get an error.
+			return cell.value
 		
+		# The empty string is invalid for columns besides text,
+		# unless we treat it as a database NULL.
+		if cell.value.strip() == "" and datatype != "text":
+			return None # db NULL
+		
+
 		# Normalize the raw value.
 		try:
-			return typ().cast(cellvalue)
+			return typ().cast(cell.value)
 		except ValueError:
 			# If normalization fails, the user has provided an
 			# invalid value.
-			raise UserError('The value "%s" in row %d column %d (%s) is invalid for a %s column.' % (cellvalue, rownum+1, colnum+1, colname, datatype))
+			raise UserError('The value "%s" in row %d column %d (%s) is invalid for a %s column.' % (cell.value, rownum+1, colnum+1, colname, datatype))
 			
 	# Utility function to convert a messytables row to a
 	# datastore API row.
@@ -430,22 +508,16 @@ def upload_resource_records(resource, schema, recorditer):
 			raise UserError("Row %d does not have %d columns." % (rownum, len(columns)))
 		row2 = { }
 		for i, col in enumerate(columns):
-			row2[col["name"]] = format_value(
-				row[i].value, col["type"],
+			row2[col["name"]] = validate_cell(
+				row[i], col["type"],
 				rownum, i, col["name"])
 		return row2
 			 
-	# Convert the schema column info into a list.
-	
-	columns = []
-	for i, col in sorted(schema["columns"].items()):
-		columns.append(col)
-			
 	# Finally, the actual procedure to chunk the rows and do
 	# the upload.
 	
 	rownum = 0
-	for rows in chunky(recorditer, 1024):
+	for rows in chunky(recorditer, 2000):
 		log.info("Uploading row %d..." % rownum)
 		
 		# Re-format messytables row into the list of dicts expected
@@ -453,7 +525,7 @@ def upload_resource_records(resource, schema, recorditer):
 		# error reporting.
 		payload = []
 		for row in rows:
-			payload.append(format_record(rownum, row, columns))
+			payload.append(format_record(rownum, row, schema["columns"]))
 			rownum += 1
 			
 		# Execute API call.
@@ -471,11 +543,24 @@ if args.resource_id == None:
 	for package_id in packages:
 		# Get the package's first resource.
 		pkg = ckan("package_show", { "id": package_id })
-		resource = pkg["resources"][0]
+		
+		# Filter out resources to skip.
+		resources = [r for r in pkg["resources"] if r["format"].lower() not in ("api","query tool")]
+		
+		# Has  aresource to upload? Get the first.
+		if len(resources) == 0:
+			log.error("Package %s has no uploadable resources." % pkg["name"])
+			continue
+		resource = resources[0]
+
+		log.info("Processing %s/resource/%s ..." % (pkg["name"], resource["id"]))
+
 		try:
 			upload_resource_to_datastore(resource)
 		except UserError as e:
 			log.error(e)
+			
+		log.info("") # blank line please
 else:
 	# Upload a particular resource.
 	resource = ckan("resource_show", { "id": args.resource_id })
